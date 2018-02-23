a8fed2ff1518cfc84f0c8de59ec88726ba427fcd
dobrovolskyi <ihor.dobrovolskyi@gmail.com>
2018-02-23 16:59:46 +0200

    add Group.java

---
+++ src/main/java/Group.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Group extends Human {
+    public Group(List<Student> list) {
+
+    }
+
+    public List<Student> fillGroup() {
+        List<Student>
+        return
+    }
+}
